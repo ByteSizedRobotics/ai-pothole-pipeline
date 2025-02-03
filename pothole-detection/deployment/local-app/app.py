624ab60b07cd9eb76bd313d6cebdf95e4331475a
Nathan Gawargy <nathan@gawargy.com>
2025-02-03 15:21:12 -0500

    added fixes, seems to WORK
    need to add prod server (ie. dont use FLASK for prod server)

--- pothole-detection/deployment/local-app/app.py
+++ pothole-detection/deployment/local-app/app.py
@@ -120,4 +120,4 @@ def start_live_inference():
 
 if __name__ == '__main__':
     load_model("yolo5s")  # CHANGE THE MODEL WANT TO LOAD HERE
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
