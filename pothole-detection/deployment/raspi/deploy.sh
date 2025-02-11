0ea88961dcda46f84fe4e5d60f7434a268204790
TheHiberNate <nathan@gawargy.com>
2025-02-11 10:21:43 -0500

    modify dockerfile and bash script

--- pothole-detection/deployment/raspi/deploy.sh
+++ pothole-detection/deployment/raspi/deploy.sh
@@ -1,16 +1,17 @@
 # Scripts to deploy the Flask app locally using Docker
-# NOTE: Docker Desktop needs to be running
 
 #!/bin/bash
 
 IMAGE_NAME="ngawargy/pothole-classifier-app"
+SAVED_IMAGES_PATH_CONTAINER="/app/saved_images"
+SAVED_IMAGES_PATH_HOST
 
 # TODO: you need to specifiy the directory where you want to share files between the host and the container
 # this directory will be used to store the uploaded images and so container has access to it
 CONTAINER_NAME="flask-pothole"
 
 echo "Pulling the latest Docker image"
-docker pull $IMAGE_NAME
+docker pull docker pull --platform linux/arm64 $IMAGE_NAME
 
 # stop and remove any existing container with the same name
 if [ $(docker ps -aq -f name=$CONTAINER_NAME) ]; then
@@ -25,6 +26,7 @@ docker run -d \
     --name $CONTAINER_NAME \
     --device=/dev/video0 \
     -p 5000:5000 \
+    -v /path/to/shared/directory:$SAVED_IMAGES_PATH_CONTAINER \
     $IMAGE_NAME
 
 echo "Deployment script complete. Access the app at http://localhost:5000"
